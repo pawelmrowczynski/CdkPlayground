2ee91d401cff6b91c0617470d75140fa30382df1
pawel.mrowczynski <pawel.mrowczynski@outlook.com>
2019-04-01 22:44:39 +0200

    First resource

--- lib/cdk_playground-stack.ts
+++ lib/cdk_playground-stack.ts
@@ -1,4 +1,5 @@
 import cdk = require('@aws-cdk/cdk');
+import iam = require('@aws-cdk/aws-iam')
 
 export class CdkPlaygroundStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
